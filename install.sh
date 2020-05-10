90ea796a964571e04c8da6d5245f0ebe213013d3
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-05-10 22:55:26 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ wget -O inter.zip https://fonts.google.com/download?family=Inter
 mkdir inter
 unzip inter.zip -d inter
 sudo mkdir /usr/share/fonts/Inter
-sudo mv ./inter/static/Inter-*-slnt=0.ttf ./inter/static/Inter-*.ttf
 sudo mv ./inter/static/*.ttf /usr/share/fonts/Inter/
 rm -r inter
 rm inter.zip
