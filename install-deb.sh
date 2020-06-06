c88bba4837d0af7640fd34f3115d151a15424180
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-06-06 17:10:01 +0900

    Create install-deb.sh

---
+++ install-deb.sh
@@ -0,0 +1,12 @@
+cd inter
+mkdir usr
+cd usr
+mkdir share
+cd share
+wget -O inter.zip https://fonts.google.com/download?family=Inter
+mkdir inter
+unzip inter.zip -d inter
+rm inter.zip
+cd ../../..
+dpkg -b inter
+sudo apt install ./inter.deb
