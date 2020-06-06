0dabc8287a700f135ec7541de7de5326cc277883
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-06-06 19:00:05 +0900

    Update install-deb.sh

--- install-deb.sh
+++ install-deb.sh
@@ -7,6 +7,10 @@ cd share
 wget -O inter.zip https://fonts.google.com/download?family=Inter
 mkdir inter
 unzip inter.zip -d inter
+mv ./inter/static/*.ttf ./
+rm -r inter
+mkdir inter
+mv ./*.ttf inter
 rm inter.zip
 cd ../../..
 dpkg -b inter
