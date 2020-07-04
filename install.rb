9f9e5ac99948ed76d866a35679a86d289a53dd10
ピアノねこ PianoCat <61345869+NSK-1010@users.noreply.github.com>
2020-07-04 15:43:46 +0900

    Update and rename install.sh to install.rb

---
+++ install.rb
@@ -0,0 +1,35 @@
+require 'open-uri'
+@path = 'https://fonts.google.com/download?family=Inter'
+
+fileName = "inter.zip"
+
+open(fileName, 'rwa+b') do |output|
+  open(@path) do |data|
+    output.write(data.read)          #※２
+  end
+end
+
+require 'zip'
+
+dest = 'inter/'
+
+Zip::File.open('inter.zip') do |zip|
+  zip.each do |entry|
+    p entry.name
+    zip.extract(entry, dest + entry.name) { true }
+  end
+end
+
+require "fileutils"
+FileUtils.mkdir("/usr/share/fonts/Inter")
+
+require "fileutils"
+
+Dir.glob('*.ttf').each do |fontname|
+FileUtils.mv("inter/static/" + fontname, "/usr/share/fonts/Inter/" + fontname)
+
+reqiure 'fileutils'
+FileUtils.rm_rf("inter")
+
+require 'fileutils'
+FileUtils.rm("inter.zip")
--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget -O inter.zip https://fonts.google.com/download?family=Inter
-mkdir inter
-unzip inter.zip -d inter
-sudo mkdir /usr/share/fonts/Inter
-sudo mv ./inter/static/*.ttf /usr/share/fonts/Inter/
-rm -r inter
-rm inter.zip
